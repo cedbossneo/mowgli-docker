a1a6bdbde8194dbc93718f03baba2d273b0a93af
Cedric Hauber <hauber.c@gmail.com>
2023-04-06 07:00:26 +0000

    WIP

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,8 +3,11 @@ sudo chown -Rf ubuntu:users /home/ubuntu/.ros
 source /opt/ros/noetic/setup.bash
 source /home/ubuntu/open_mower_ros/devel/setup.bash
 cd /home/ubuntu/open_mower_ros
-export ROS_IP=$(/sbin/ip route|awk '/default/ { print $9 }')
-export ROS_MASTER_URI=http://localhost:11311
+if [ -z "${ROS_MASTER_URI}" ];
+then
+    export ROS_IP=$(/sbin/ip route|awk '/default/ { print $9 }')
+    export ROS_MASTER_URI=http://localhost:11311
+fi
 export CATKIN_SHELL=bash
 
 exec $*
