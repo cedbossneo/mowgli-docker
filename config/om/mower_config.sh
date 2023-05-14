4cca5fff51a8dbaa717cf966a9126786ed814a93
Cedric Hauber <hauber.c@gmail.com>
2023-05-14 13:32:26 +0000

    Fix mower config

--- config/om/mower_config.sh
+++ config/om/mower_config.sh
@@ -104,3 +104,13 @@ export OM_AUTOMATIC_START=false
 export OM_OUTLINE_OFFSET=0.05
 
 export OM_NO_COMMS=true
+
+# Set default GPS antenna offset
+export OM_ANTENNA_OFFSET_X=0.3
+export OM_ANTENNA_OFFSET_Y=0.0
+
+# Set distance between wheels in m
+export OM_WHEEL_DISTANCE_M=0.325
+
+# Set default ticks/m
+export OM_WHEEL_TICKS_PER_M=300.0
