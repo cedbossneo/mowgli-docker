e6fa48cb0deee8f32031fd6f95327786bf817634
Cedric Hauber <hauber.c@gmail.com>
2023-07-09 18:08:43 +0000

    Quick build fix

--- tools/rviz.sh
+++ tools/rviz.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --net=host -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY -h $HOSTNAME -v $HOME/.Xauthority:/root/.Xauthority ghcr.io/cedbossneo/mowgli-docker:cedbossneo-arm64 roslaunch open_mower remote_rviz.launch
+docker run --net=host -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY -h $HOSTNAME -v $HOME/.Xauthority:/root/.Xauthority ghcr.io/cedbossneo/mowgli-docker:cedbossneo roslaunch open_mower remote_rviz.launch
