2fb3bd98fd4f1a9e5e092dc180d4604416b172c5
Cedric Hauber <hauber.c@gmail.com>
2024-10-12 15:37:06 +0200

    Update patch.sh

--- docker/openmower/patch.sh
+++ docker/openmower/patch.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 set -xe
-echo "Patching OM"
-sed -i 's/output="screen"/unless="\$\(optenv OM_NO_COMMS True\)" output="screen"/g' /opt/open_mower_ros/src/open_mower/launch/include/_comms.launch
-
 echo "Patching for implausible wheel ticks"
 
 
